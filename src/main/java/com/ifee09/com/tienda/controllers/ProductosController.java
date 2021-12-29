4857a75b770f25791f46ee5f73c9212083fe4e50
Maynor2000 <a20216387@alumnos.uady.mx>
2021-12-29 14:04:26 -0600

    images

--- src/main/java/com/ifee09/com/tienda/controllers/ProductosController.java
+++ src/main/java/com/ifee09/com/tienda/controllers/ProductosController.java
@@ -1,26 +1,24 @@
 package com.ifee09.com.tienda.controllers;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.time.LocalDate;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.ifee09.com.tienda.models.Productos;
 import com.ifee09.com.tienda.repositorys.ProductosRespository;
+import com.ifee09.com.tienda.services.Image;
 
 @RestController
 @RequestMapping("/productos")
@@ -45,29 +43,48 @@ public class ProductosController {
 		return EntityModel.of(pr.findById(id));
 	}
 	@PostMapping("")
-	public EntityModel<?> posProductos(@ModelAttribute Productos p,MultipartFile file,HttpServletRequest request) {
-		
-		try {
+	public EntityModel<?> posProductos(@ModelAttribute Productos p,MultipartFile file
+			,HttpServletRequest hsr) {
+		//crea un nombre para la imagen con formato
+		String nombreimagen = Image.createNameImage(p);
+		//guarda la imagen en el disco
+		Image.writeImage(file,nombreimagen);
+		//coloca la ruta de la imagen
+		p.setFoto(Image.createRuteImage(nombreimagen,hsr));
+		//coloca una fecha a la imagen
+		p.setFecha(LocalDate.now());
 			
-			byte[]data = file.getBytes();
-			
-			FileOutputStream fot = new FileOutputStream("src/main/resources/static/images/"+p.getNombre()+".jpg");
-			
-			fot.write(data);
-			fot.close();
-			
-			File ruta = new File("/static/images/"+p.getNombre()+".jpg");
-			p.setFoto(ruta.getAbsolutePath());
-			p.setFecha(LocalDate.now());
-			System.out.println(request.getRemoteHost());
-			
-			
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;//EntityModel.of(pr.save(p));
+		return EntityModel.of(pr.save(p));
 	}
-
+	@PutMapping("/{id}")
+	public EntityModel<?> updateProductos(@PathVariable("id") Integer id
+			,@ModelAttribute Productos p,MultipartFile mf){
+		return pr.findById(id).map(data->{
+			data.setNombre(p.getNombre());
+			//buscamos la imagen anterior y la borramos
+			String nombreImage = Image.getNameImage(data.getFoto());//retorna el nombre de la imagen con formato
+			Image.deleteImage(nombreImage);//elimanos la imagen
+			Image.writeImage(mf,nombreImage);//escribimos la nueva imagen en el disco con el mismo nombre
+			//-----------------------------------------
+			data.setPrecio(p.getPrecio());
+			data.setDescripcionC(p.getDescripcionC());
+			data.setDescripcionL(p.getDescripcionL());
+			return EntityModel.of(pr.save(data));
+		}).orElseGet(()->{
+			return EntityModel.of(pr.save(p));
+		});
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?>deleteProductos(@PathVariable("id") Integer id){
+		//buscamos la entidad por id para ser eliminada
+		Productos p = pr.findById(id).get();
+		//obtenemos el nombre con fromato de la imagen
+		String nombreImagen = Image.getNameImage(p.getFoto());
+		//si la imagen fue eliminada eliminamos la entidad
+		if(Image.deleteImage(nombreImagen)) {
+			pr.delete(p);
+			//retornamos el codigo http
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		}else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
 }
---
+++ src/main/java/com/ifee09/com/tienda/services/Image.java
@@ -0,0 +1,67 @@
+package com.ifee09.com.tienda.services;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ifee09.com.tienda.models.Productos;
+
+public class Image {
+	
+	public Image() {
+		
+	}
+	public static String createNameImage(Productos p) {
+		//nombre de la imagen con formato
+		String nombre = p.getNombre()+"-1";
+		String formato = ".png";
+		//validamos si el nombre de la imagen ya existe
+		File isruta = new File("src/main/resources/static/images/"+nombre+formato);
+		//si el nombre existe le suma 1 a su numero
+		if(isruta.exists()) {
+			while(isruta.exists()) {
+				String [] imagen = nombre.split("-");
+				nombre = imagen[0]+"-"+(Integer.parseInt(imagen[1])+1);
+				isruta = new File("src/main/resources/static/images/"+nombre+formato);
+			}
+		}
+		return nombre+formato;
+	}
+	public static boolean writeImage(MultipartFile file,String imagen) {
+		try {
+			byte[] data = file.getBytes();
+			//inserta la imagen en el disco
+			FileOutputStream fot = new FileOutputStream("src/main/resources/static/images/"+imagen);
+			fot.write(data);
+			fot.close();
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public static boolean deleteImage(String imagen){
+		//inserta la imagen en el disco
+		File fot = new File("src/main/resources/static/images/"+imagen);
+		if(fot.delete())return true;
+		else return false;
+		
+	}
+	public static String createRuteImage(String imagen,HttpServletRequest hsr) {
+		//crea una ruta para la imagen en el servidor
+		File ruta = new File("/images/"+imagen);
+		String[] hostName = hsr.getLocalAddr().split("/");
+		
+		return  hostName[0]+ruta.getAbsolutePath();
+	}
+	public static String getNameImage(String rutaImagenHost) {
+		String[] rutasImage = rutaImagenHost.split("/");
+		
+		return rutasImage[2];
+	}
+}
